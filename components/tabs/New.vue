8f18fa4b4bfca9a19ec63ac52fa7eac64bc1ec54
Brian <brianelblog@gmail.com>
2018-07-23 23:09:32 -0300

    adding loading, disable send btn and fix error input

--- components/tabs/New.vue
+++ components/tabs/New.vue
@@ -1,21 +1,23 @@
 <template>
     <nb-container>
         <nb-content padder>
-            <nb-form>
-                <nb-item stackedLabel>
-                <nb-label>Ingresa algun producto que falte</nb-label>
-                <nb-input v-model="name" placeholder="Papel higienico..." />
-                <nb-button block primary v-bind:onPress="sendNew">
-                    <nb-text>Enviar</nb-text>
-                </nb-button>
+                <nb-item :error="error">
+                    <nb-input placeholder='Papel higienico...' v-model="name" />
+                    <nb-icon name="close-circle" v-if="error" />
                 </nb-item>
-            </nb-form>
+            <View padder class="sendView">
+                <nb-button full primary v-bind:onPress="sendNew" :disabled="disableSend" >
+                    <nb-text >Enviar</nb-text>
+                </nb-button>
+            </View>
+            <nb-spinner color="green" v-if="loading"/>            
         </nb-content>
+            
     </nb-container>
 </template>
 <script>
 import React from "react"
-import { Toast } from "native-base"
+import { Toast, Spinner } from "native-base"
 import {AsyncStorage} from "react-native"
 import ItemsService from '../../services/Items'
 
@@ -25,17 +27,36 @@ export default {
     data() {
         return {
             name: '',
+            loading: false,
+            error: false
+        }
+    },
+    computed: {
+        disableSend() {
+            return this.loading || this.error || this.name === ''
+        }
+    },
+    watch: {
+        name: function() {
+            if(this.name == '') {
+                this.error = true
+            } else {
+                this.error = false
+            }
         }
     },
     methods: {
         sendNew: function() {
             _storeData = async () => {
+                this.loading = true
                 try {
-                    await ItemsService.addProduct({name: this.name});
-                    this.addedSuccess()                                
+                    await ItemsService.addProduct({name: this.name.trim()});
+                    this.addedSuccess()
+                    this.loading = false                   
                 } catch (error) {
                     this.onError()
                     console.warn(error)
+                    this.loading = false
                 }
             }
             _storeData()
@@ -60,3 +81,9 @@ export default {
     }
 }
 </script>
+
+<style>
+.sendView {
+    margin-top: 20px;
+}
+</style>
